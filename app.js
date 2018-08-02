49400fffec79db14026d23d4982b4f1b246cb88b
Dominic Zappala <BB_Boy@Monicas-MacBook-Pro.local>
2018-08-01 22:24:49 -0400

    changes to app

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ import ReactDom from 'react-dom';
 export const SocialCard = () => {
     <div>
         <h1>This is an H1 element</h1>
+        <h2>This is an H2 element</h2>
+        <p>This is a paragraph<p>
     </div>
 }
